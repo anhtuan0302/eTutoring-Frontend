2608a21c732d0a0e1d5a72ffe89ab6cb54c0df99
Tuan Ngo Anh <19220963@st.neu.edu.vn>
2025-03-17 13:25:21 +0700

    Initialize ReactJS project

---
+++ src/components/layouts/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <p>Copyright &copy; 2025 University of Greenwich</p>
+        </FooterContainer>
+    )
+}
+
+export default Footer;
