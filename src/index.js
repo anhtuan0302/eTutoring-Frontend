f0b00b438344497ce505c0a84e1d3f7fc6892079
Tuan Ngo Anh <19220963@st.neu.edu.vn>
2025-04-01 18:27:08 +0700

    Add ConfigProvider for index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={{ hashed: false }}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
